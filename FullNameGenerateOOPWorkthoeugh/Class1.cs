5550342b348bb5b074229fa4068f9d136e3b5e01
floatingFaez <floatingfaez@live.com>
2014-09-26 22:52:03 +0600

    Full Project File Initially Uploaded

---
+++ FullNameGenerateOOPWorkthoeugh/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FullNameGenerateOOPWorkthoeugh
+{
+    public class EntryPoint
+    {
+        public static void Main()
+        {
+            Person aPerson = new Person();
+
+            aPerson.firstName = "Abul";
+            aPerson.middleName = "Faez";
+            aPerson.lastName = "Sajib";
+
+            Console.WriteLine("Your full name is : " + aPerson.GetFullName());
+            Console.WriteLine("Your reverse name is : " + aPerson.GetFullReverseName());
+
+            Console.ReadKey();
+        }
+    }
+}
