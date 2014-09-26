976ebc5cf6f29d1fbe8dd9185439c0b5cac7e555
floatingFaez <floatingfaez@live.com>
2014-09-26 23:03:40 +0600

    User Take Data From User

--- FullNameGenerateOOPWorkthoeugh/EntryPoint.cs
+++ FullNameGenerateOOPWorkthoeugh/EntryPoint.cs
@@ -11,12 +11,14 @@ namespace FullNameGenerateOOPWorkthoeugh
         public static void Main()
         {
             Person aPerson = new Person();
+            Console.Write("Enter your first name : ");
+            aPerson.firstName = Console.ReadLine();
+            Console.Write("Enter your middle name : ");
+            aPerson.middleName = Console.ReadLine();
+            Console.Write("Enter your last name : ");
+            aPerson.lastName = Console.ReadLine();
 
-            aPerson.firstName = "Abul";
-            aPerson.middleName = "Faez";
-            aPerson.lastName = "Sajib";
-
-            Console.WriteLine("Your full name is : " + aPerson.GetFullName());
+            Console.WriteLine("\nYour full name is : " + aPerson.GetFullName());
             Console.WriteLine("Your reverse name is : " + aPerson.GetFullReverseName());
 
             Console.ReadKey();
